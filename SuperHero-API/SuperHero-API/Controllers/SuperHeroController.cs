3bbbab716c34f7a4eedcc4dfcb2223beb5433169
muhyasin89 <muhyasin89@gmail.com>
2023-06-02 14:15:08 +0700

    [main]add main

---
+++ SuperHero-API/SuperHero-API/Controllers/SuperHeroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHero_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHero() 
+        {
+            return new List<SuperHero>{
+                new SuperHero {
+                    Name="Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+        }
+    }
+}
